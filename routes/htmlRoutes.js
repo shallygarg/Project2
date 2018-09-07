1e2f80559da805e9d77971065375ce2ee43475f1
Grant Yunker <grantyunker@gmail.com>
2018-09-06 20:13:11 -0700

    Made the associations prettier

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,7 +13,9 @@ module.exports = function(app) {
 
   // Load thread page and pass in a thread by id
   app.get("/thread/:id", function(req, res) {
-    db.Thread.findOne({ where: { id: req.params.id },include: [db.Comment]}).then(function(data) {
+    db.Thread.findOne({ 
+      where: { id: req.params.id },
+      include: [db.Comment]}).then(function(data) {
       res.render("thread", {
         thread: data
       });
@@ -22,7 +24,7 @@ module.exports = function(app) {
 
   // Render locations
   app.get("/locations", function(req, res) {
-    db.Thread.findOne({ where: { id: req.params.id } }).then(function(data) {
+    db.Thread.findOne({ where:{ id: req.params.id } }).then(function(data) {
       res.render("locations", {
         location: data
       });
