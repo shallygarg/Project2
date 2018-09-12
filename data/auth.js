4ba90206f95ae118af8b354f5c79b2dc1f416fb9
Shally Garg <garg.shally05@gmail.com>
2018-09-11 20:51:13 -0700

    fixing eslint issues

--- data/auth.js
+++ data/auth.js
@@ -2,7 +2,7 @@ var jwt = require("jsonwebtoken");
 var cfg = require("../data/config.js");
 
 function verifyToken(req, res, next) {
-  const token = req.query.token || req.headers.token //token change
+  var token = req.query.token || req.headers.token; //token change
   console.log(token + "------------------------------");
   console.log(cfg.jwtSecret + "   secret");
   jwt.verify(token, cfg.jwtSecret, function(err, decodedToken) {
@@ -10,8 +10,7 @@ function verifyToken(req, res, next) {
     console.log(decodedToken + "--decodedcoded");
     req.user = decodedToken;
     next();
-  })
-
+  });
 }
 
-module.exports = {verifyToken: verifyToken};
+module.exports = { verifyToken: verifyToken };
